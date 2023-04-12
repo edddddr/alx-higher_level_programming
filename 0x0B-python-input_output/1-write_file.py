78b89912dd502a575c33bbc114f91747325e2b28
edddddr <endt3642@gmail.com>
2023-04-11 18:49:57 -0700

    python-input_output

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+
+#!/usr/bin/python3
+"""This module defines the write_file function"""
+
+
+def write_file(filename="", text=""):
+    """write a file
+    Args:
+      filename(str): Filename
+      text(str): Text written on file
+    """
+    with open(filename, 'w') as file:
+        fileW = file.write(text)
+        file.close
+    return len(text)
