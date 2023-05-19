c1054ea81d854c799a301e701948ad505cf14654
endrias <97146464+edddddr@users.noreply.github.com>
2023-05-18 21:39:17 -0700

    mandatory

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,4 +1,4 @@
---Lists all shows and genres linked to the show from the
+-- Lists all shows and genres linked to the show from the
 -- database hbtn_0d_tvshows.
 -- Records are ordered by ascending show title and genre name.
 SELECT t.`title`, g.`name`
